4d08bf77acbe2ea397395d8e721bcaad576825d0
Miguel Setúbal <migueljonas@alu.ufc.br>
2022-06-02 00:29:52 -0300

    add casos de teste - cadastro

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -1,21 +1,10 @@
+/// <reference types="cypress"/>
+
 describe('POST /characters', function () {
 
     before(function () {
-
-        const login = {
-            email: "",
-            password: ""
-        }
-
-        cy.request({
-            method: 'POST',
-            url: '/sessions',
-            body: login
-        }).then(function(response){
-            expect(response.status).to.be.equal(200)
-            cy.log(response.body.token)
-            Cypress.env('token', response.body.token)
-        })
+        cy.limparAmbiente()
+        cy.login()
     })
 
     it('Deve cadastrar um personagem', function () {
@@ -23,19 +12,35 @@ describe('POST /characters', function () {
             name: "Charles Chavier",
             alias: "Professor X",
             team: ["x-main", "a"],
-            active: false
+            active: true
         }
-
-        cy.request({
-            method: 'POST',
-            url: '/characters',
-            headers:{
-                Authorization: Cypress.env('token')
-            },
-            body: character
-        }).then(function (response) {
+        cy.cadastraPersonagen(character).then(function (response) {
             expect(response.status).to.be.equal(201)
+            expect(response.body.character_id).not.be.null
         })
 
+    })
+
+    context('dado que um personagem já existe', function () {
+
+        const character = {
+            name: "Wanda",
+            alias: "Feitiseira",
+            team: ["vingadores", "vila"],
+            active: true
+        }
+
+        before(function () {
+            cy.cadastraPersonagen(character).then(function (response) {
+                expect(response.status).to.be.equal(201)         
+            })
+        })
+
+        it('não deve cadastrar duplicado', function () {
+            cy.cadastraPersonagen(character).then(function (response) {
+                expect(response.status).to.be.equal(400)  
+                expect(response.body.error).to.be.equal("Duplicate character")      
+            })
+        })
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,41 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('limparAmbiente', function(){
+    cy.request({
+        method: 'DELETE',
+        url: '/back2thepast/62977773fd16d2001677e1a3',
+    }).then(function(response){
+        expect(response.status).to.be.equal(200)            
+    })
+})
+
+Cypress.Commands.add('login', function(){    
+    const login = {
+        email: "",
+        password: ""
+    }
+
+    cy.request({
+        method: 'POST',
+        url: '/sessions',
+        body: login
+    }).then(function(response){
+        expect(response.status).to.be.equal(200)        
+        Cypress.env('token', response.body.token)
+    })
+})
+
+Cypress.Commands.add('cadastraPersonagen', function(payload){
+    cy.request({
+        method: 'POST',
+        url: '/characters',
+        headers: {
+            Authorization: Cypress.env('token')
+        },
+        body: payload, 
+        failOnStatusCode: false 
+    }).then(function(response){
+        return response
+    })
+})
